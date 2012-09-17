302300759d9f96a2101c0dff0eade158c8cee25d
Madhu GB <madhuvana@gmail.com>
2012-09-17 09:38:50 +0530

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2012 Madhu GB <madhu@madspace.me>,
+ * Copyright 2012 Madhu GB <madhuvana@gmail.com>,
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
